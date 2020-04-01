cd0e95fdf8e7b383278e1057eaa0a27974173b16
guillsav <guillsav@gmail.com>
2020-04-01 14:02:22 -0400

    Created Navbar component and added FontAwesome to project.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { fab } from "@fortawesome/free-brands-svg-icons"
+import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons"
+
+import Navbar from "./components/Navbar/Navbar.component"
 
 import "./App.css"
 
+library.add(fab, faCheckSquare, faCoffee)
+
 class App extends Component {
   render() {
-    return <h1>App</h1>
+    return (
+      <div className="App">
+        <Navbar />
+      </div>
+    )
   }
 }
 
